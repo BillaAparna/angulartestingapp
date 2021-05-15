c7fadf7b44ffe2773d1a17c6eb78985a3db3482b
aparna <aparnabilla.09@gmail.com>
2021-05-15 16:59:41 +0530

    routing and navigation done

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent {
 
- products=[1,2,3,4,5,6]
-
+ //products=[1,2,3,4,5,6]
+ products=[
+   {
+    productTitle:"Tech-1",
+    productImage:"",
+    description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis voluptatum adipisci amet! Illo nesciunt nisi consequatur explicabo cumque quaerat cum, id earum blanditiis unde, ratione, asperiores eum hic! Suscipit, nobis."
+  },
+  {
+    productTitle:"Tech-2",
+    productImage:"",
+    description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis voluptatum adipisci amet! Illo nesciunt nisi consequatur explicabo cumque quaerat cum, id earum blanditiis unde, ratione, asperiores eum hic! Suscipit, nobis."
+  },
+  {
+    productTitle:"Tech-3",
+    productImage:"",
+    description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis voluptatum adipisci amet! Illo nesciunt nisi consequatur explicabo cumque quaerat cum, id earum blanditiis unde, ratione, asperiores eum hic! Suscipit, nobis."
+  },
+  {
+    productTitle:"Tech-4",
+    productImage:"",
+    description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis voluptatum adipisci amet! Illo nesciunt nisi consequatur explicabo cumque quaerat cum, id earum blanditiis unde, ratione, asperiores eum hic! Suscipit, nobis."
+  },
+  {
+    productTitle:"Tech-5",
+    productImage:"",
+    description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis voluptatum adipisci amet! Illo nesciunt nisi consequatur explicabo cumque quaerat cum, id earum blanditiis unde, ratione, asperiores eum hic! Suscipit, nobis."
+  },
+  {
+    productTitle:"Tech-6",
+    productImage:"",
+    description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis voluptatum adipisci amet! Illo nesciunt nisi consequatur explicabo cumque quaerat cum, id earum blanditiis unde, ratione, asperiores eum hic! Suscipit, nobis."
+  },
+]
 }
