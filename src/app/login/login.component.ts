b22fb4055cffd71da8b87a0ff341610e562a799e
aparna <aparnabilla.09@gmail.com>
2021-05-16 08:31:41 +0530

    nestedrouting completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit,OnDestroy{
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngOnDestroy(){
+    console.log("destroyed login");
   }
 
 }
